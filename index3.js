64924f1388f9d38c2319fdad1394d55c1934871c
Ardiubl <arditamajuliansyah@gmail.com>
2021-04-20 02:57:47 +0700

    projek 4

---
+++ index3.js
@@ -0,0 +1,12 @@
+// object
+
+const dataMahasiswa = {
+    nama : 'arditamajuliansyah',
+    npm : '19421053',
+    alamat: {
+        rt : '01',
+        rw : '99'
+    }
+}
+
+console.log(dataMahasiswa.alamat.rt)
